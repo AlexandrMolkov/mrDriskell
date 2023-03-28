f9104b5ed4710b616a9d885f3f9f2437d768b33c
AlexandrMolkov <alexandr.molkov95@gmail.com>
2023-03-28 17:26:45 +0300

    catalog, brands, 404

--- src/js/common/main.js
+++ src/js/common/main.js
@@ -14,13 +14,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
     document.querySelectorAll('.btn').forEach( btn => {
 
-        const decL = document.createElement('span')
-        decL.classList.add('btn__dec-left')
-        btn.append(decL)
-        const decR = document.createElement('span')
-        decR.classList.add('btn__dec-right')
-        btn.append(decR)
-        
+        if(!btn.classList.contains('btn_cat')) {
+            const decL = document.createElement('span')
+            decL.classList.add('btn__dec-left')
+            btn.append(decL)
+            const decR = document.createElement('span')
+            decR.classList.add('btn__dec-right')
+            btn.append(decR)
+        }
+
     } )
 
 
